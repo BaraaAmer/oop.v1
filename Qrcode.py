e9abafc0b43372b643396150436dda3783052282
BaraaAmer <137370407+BaraaAmer@users.noreply.github.com>
2023-07-19 15:01:16 +0300

    Update Qrcode.py

--- Qrcode.py
+++ Qrcode.py
@@ -1,14 +1,22 @@
 import qrcode
 from art import *
 
-text = "Qr Code"
-font = "slant"
+text = " Qr Code"
+font = "smslant"
 
 art = text2art(text, font=font)
 print(art)
 
-image = qrcode.make(input("Enter your Url --> :"))
+image = qrcode.make(input(" Enter your Url --> :"))
 
-image.save(input("Enter path to save and new name --> : "))
+print("=" * 40)
+
+text = "set path : /sdcard/name.png"
+
+print(text.center(40 , "="))
+
+print("=" * 40)      
+
+image.save(input(" Enter path to save and new name --> : "))
 
 print("Done {+}")
